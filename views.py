0c5f2496674dbdd62401e856b3f4565b888fce79
manager <root@DESKTOP-4VRGT5T.localdomain>
2020-11-20 11:05:07 +0800

    manager添加number5

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ number4 = 402
 =======
 number4 = 401
 >>>>>>> ed7c6eb11046c5ffda47b7bf6eca8553a5e9d5f1
-
+number5 = 501
 Enumerating objects: 4, done.
 Enumerating objects: 4, done.
 Counting objects: 100% (4/4), done.
